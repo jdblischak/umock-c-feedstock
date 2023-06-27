e24e70dcda93a09db67b00120d52fea9e3fa243a
John Blischak <jdblischak@gmail.com>
2023-06-27 17:06:22 -0400

    Simplify build scripts

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,17 +1,13 @@
 #!/usr/bin/env bash
 set -e
 
-mkdir -p build
 cd build
 
 cmake \
     -D CMAKE_INSTALL_PREFIX=${PREFIX} \
     -D CMAKE_PREFIX_PATH=${PREFIX} \
-    -D CMAKE_BUILD_TYPE=Release \
-    -D CMAKE_INSTALL_SYSTEM_RUNTIME_LIBS_SKIP=True \
-    -D BUILD_SHARED_LIBS=ON \
     -D use_installed_dependencies=ON \
-    ${SRC_DIR}
+    ..
 
 make -j$CPU_COUNT
 make install
