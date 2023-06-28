ef93a95b5785cda4455315d03e26b8b240f9337a
John Blischak <jdblischak@gmail.com>
2023-06-28 16:23:08 -0400

    Attempt to run tests as part of the recipe build

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,7 +3,10 @@
 cmake -S . -B build %CMAKE_ARGS% ^
   -G "Ninja" ^
   -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
-  -D use_installed_dependencies=ON
+  -D use_installed_dependencies=ON ^
+  -D run_valgrind:BOOL=ON ^
+  -D run_unittests:BOOL=ON ^
+  -D run_int_tests:BOOL=ON
 if errorlevel 1 exit 1
 
 cmake --build build --target install
--- recipe/build.sh
+++ recipe/build.sh
@@ -2,6 +2,9 @@
 set -eux
 
 cmake -S . -B build $CMAKE_ARGS \
-  -D use_installed_dependencies=ON
+  -D use_installed_dependencies=ON \
+  -D run_valgrind:BOOL=ON \
+  -D run_unittests:BOOL=ON \
+  -D run_int_tests:BOOL=ON
 
 cmake --build build -j$CPU_COUNT --target install
