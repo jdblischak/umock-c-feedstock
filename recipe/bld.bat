b02e4179e378d3f6a058be4a11241c30265ee617
John Blischak <jdblischak@gmail.com>
2023-06-28 14:49:47 -0400

    Troubleshoot CMAKE_PREFIX_PATH on Windows

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,12 +1,16 @@
 @echo on
 
+echo CONDA_BUILD=%CONDA_BUILD%
+echo CMAKE_ARGS=%CMAKE_ARGS%
+echo CMAKE_PREFIX_PATH=%CMAKE_PREFIX_PATH%
+
 cmake -S . -B build ^
   -G "Ninja" ^
   -D use_installed_dependencies=ON ^
   %CMAKE_ARGS%
 if errorlevel 1 exit 1
 
-type build/CMakeCache.txt
+type build\CMakeCache.txt
 
 cmake --build build --target install
 if errorlevel 1 exit 1
