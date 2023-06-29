d2195ab357003a20225175a1b65334d3f0434b6b
John Blischak <jdblischak@gmail.com>
2023-06-29 15:59:57 -0400

    Revert "Attempt to run tests as part of the recipe build"
    This reverts commit ef93a95b5785cda4455315d03e26b8b240f9337a.

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,9 +2,6 @@
 set -eux
 
 cmake -S . -B build $CMAKE_ARGS \
-  -D use_installed_dependencies=ON \
-  -D run_valgrind:BOOL=ON \
-  -D run_unittests:BOOL=ON \
-  -D run_int_tests:BOOL=ON
+  -D use_installed_dependencies=ON
 
 cmake --build build -j$CPU_COUNT --target install
